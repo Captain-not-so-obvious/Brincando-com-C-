b63ae1f950040ac08580097251c19978f1a648c6
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-10-16 17:24:07 -0300

    Create DigiteDoisNumeros.cpp

---
+++ DigiteDoisNumeros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int numero1, numero2;
+
+    std::cout << "Digite o primeiro número inteiro: ";
+    std::cin >> numero1;
+
+    std::cout << "Digite o segundo número inteiro: ";
+    std::cin >> numero2;
+
+    std::cout << "Você digitou os números: " << numero1 << " e " << numero2 << std::endl;
+
+    return 0;
+}
