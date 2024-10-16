7c4ae6420369b1a500e1693945876d7ee4100fae
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-10-16 17:22:03 -0300

    Create digiteUmNumero.cpp

---
+++ digiteUmNumero.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int numero;
+
+    std::cout << "Digite um número inteiro: ";
+    std::cin >> numero;
+
+    std::cout << "Você digitou o número: " << numero << std::endl;
+
+    return 0;
+}
