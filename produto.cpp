dec6764e4ba4b60de090d3e5234ca585f206ffb7
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-10-16 17:16:22 -0300

    Create produto.cpp

---
+++ produto.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a = 28;
+    int b = 43;
+    int produto = a * b;
+
+    std::cout << "O produto de " << a << " e " << b << " é: " << produto << std::endl;
+    return 0;
+}
